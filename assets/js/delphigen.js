d351433776e2630442e32325555f9b47f56271da
Malcolm Anderson <malcolminyo@gmail.com>
2021-10-19 21:05:27 -0700

    Added Delphi DIY

---
+++ assets/js/delphigen.js
@@ -0,0 +1,5 @@
+function makeDelphi()
+{
+    document.getElementById('input').innerText = document.getElementById('inputText').value;
+    document.getElementById('output').innerText = document.getElementById('outputText').value;
+}
